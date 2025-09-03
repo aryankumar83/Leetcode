737a9a771f2bbd01b7ab807ab4c59b0b7c782cad
Aryan Kumar <137225871+aryankumar83@users.noreply.github.com>
2025-09-03 16:14:40 +0530

    Time: 144 ms (21.22%), Space: 124 MB (18.36%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n = candyType.size();
+        set<int> s;
+        for(int i=0 ; i<n ; i++){
+            s.insert(candyType[i]);
+        }
+        int cnt=0;
+        for(auto i : s){
+            cnt++;
+        }
+        int ans = min(n/2 , cnt);
+        return ans;
+    }
+};
