52298f3613846d8ef0f2e741744762fa01a70bca
Aryan Kumar <137225871+aryankumar83@users.noreply.github.com>
2025-09-12 20:27:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.65%) - LeetHub

--- 0944-smallest-range-i/0944-smallest-range-i.cpp
+++ 0944-smallest-range-i/0944-smallest-range-i.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        sort(nums.begin() , nums.end());
-        nums[0] +=k;
-        nums[nums.size()-1] -=k;
-        return max(nums[nums.size()-1]-nums[0] , 0);
+        int mini=INT_MAX , maxi=INT_MIN;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i]>maxi)maxi=nums[i];
+            if(nums[i]<mini)mini=nums[i];
+        }
+        mini +=k;
+        maxi -=k;
+        return max(maxi-mini , 0);
     }
 };
