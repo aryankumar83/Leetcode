684b7b6b82c57f1aacca6939eace921c87cf30a4
Aryan Kumar <137225871+aryankumar83@users.noreply.github.com>
2025-09-16 04:15:44 +0530

    Time: 11 ms (26.95%), Space: 24.2 MB (13.26%) - LeetHub

--- 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
+++ 1829-maximum-units-on-a-truck/1829-maximum-units-on-a-truck.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int maximumUnits(vector<vector<int>>& b, int t) {
         for(auto &i:b)swap(i[1],i[0]);
-        sort(b.begin() , b.end());
-        reverse(b.begin() , b.end());
-        for(auto i:b){
-            cout<<i[0]<<" "<<i[1]<<endl;
-        }
+        sort(b.rbegin() , b.rend() );
+        //reverse(b.begin() , b.end());
         int ans=0;
         for(auto i:b){
             ans += min(t , i[1])*i[0];
