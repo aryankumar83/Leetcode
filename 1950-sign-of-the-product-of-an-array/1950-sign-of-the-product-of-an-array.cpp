799b35c42633a64cc26530bd916e4b7bdf0b88c4
Aryan Kumar <137225871+aryankumar83@users.noreply.github.com>
2025-09-18 00:18:06 +0530

    Time: 0 ms (100%), Space: 13.9 MB (11.44%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+
+    int signFunc(int product){
+        if(product>0)return 1;
+        if(product<0)return -1;
+        return 0;
+    }
+    int arraySign(vector<int>& nums) {
+        long long p=1;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i] < 0)p*=-1;
+            if(nums[i] == 0)p=0;
+        }
+        return signFunc(p);
+    }
+};
